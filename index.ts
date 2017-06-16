96c71b13448f01407b1d2ce3dd38f3fd3b7ffb8b
Zach Zundel <me@zachzundel.com>
2017-06-16 14:56:48 +0100

    Added support for configuration file

--- index.ts
+++ index.ts
@@ -8,7 +8,12 @@
  */
 
 import { app } from './src/app';
+import { Config } from './src/lib/config';
+
+let config = new Config();
 
 let server = app();
 
-server.listen(9999);
+let port = config.get('applicationPort');
+
+server.listen(port);
---
+++ src/lib/config.ts
@@ -0,0 +1,69 @@
+/* SynBioHub Federator
+ * Web of Registries
+ *
+ * Configuration file manager
+ *
+ * Written by Zach Zundel
+ * 16-06-2017
+ */
+
+import *  as fs from 'fs'
+
+// Default file locations
+const configFilename = './config.json';
+const configLocalFilename = './config.local.json';
+
+// Abstract schema of configuration files
+class ConfigSchema {
+    [key: string]: Object | String | Number;
+}
+
+// Represents 'merged' config class
+class Config {
+    private config: ConfigSchema;
+    private configLocal: ConfigSchema;
+
+    public constructor() {
+        let configString = fs.readFileSync(configFilename, 'utf8');
+        let configLocalString = fs.readFileSync(configLocalFilename, { encoding: 'utf8', flag: 'a+' });
+
+        this.config = JSON.parse(configString);
+        this.configLocal = configLocalString === "" ? {} : JSON.parse(configLocalString);
+    }
+
+    // Get a value from the configuration object
+    public get(key: string): Object | String | Number {
+        let preset = this.config[key];
+        let local = this.configLocal[key];
+
+        // Prefer to return local, but return preset if no local value
+        if(local === undefined) {
+            return preset;
+        }
+
+        return local;
+    }
+
+    // Set a key -- changing the local config
+    public set(key: string, value: Object | String | Number): void {
+        // Do nothing if there's no change.
+        if(this.configLocal[key] === value) {
+            return
+        }
+
+        this.configLocal[key] = value;
+
+        this.save();
+    }
+
+    // Persist changes to filesystem
+    private save(): void {
+        let configLocalString = JSON.stringify(this.configLocal, null, 4);
+
+        fs.writeFileSync(configLocalFilename, configLocalString, { encoding: 'utf8', flag: 'w' })
+    }
+}
+
+export {
+    Config
+};
